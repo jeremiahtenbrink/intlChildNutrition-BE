0b7fc240bf18120015f5037379c7f2ea726dcb19
Manju KC <Manju KC>
2019-06-23 16:01:06 -0400

    Updated knex file and added pg

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,23 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
       filename: './data/icn.db3',
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
