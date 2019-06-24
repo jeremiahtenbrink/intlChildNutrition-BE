cc65c08c7cb51325279536967602254bdb55093c
Manju KC <Manju KC>
2019-06-23 22:26:21 -0400

    Removed token since we will use session

--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const Users = require("./usermodel.js");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 router.post("/register", (req, res) => {
   let user = req.body;
@@ -30,39 +29,24 @@ router.post("/register", (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
-  console.log('i am here')
-let { username, password } = req.body;
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
 
-Users.findBy({ username })
-  .first()
-  .then(user => {
-    if (user && bcrypt.compareSync(password, user.password)) { 
-     const token = generateToken(user);
-      res.status(200).json({
-        userId: user.id,
-        userType: user.userType,
-        token: token,
-        
-      });
-    } else {
-      res.status(401).json({ message: 'I Shall not pass' });
-    }
-  })
-  .catch(error => {
-    res.status(500).json(error);
-  });
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({
+          id: user.id,
+          isAdmin: user.isAdmin
+        });
+      } else {
+        res.status(401).json({ message: "I Shall not pass" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
-
-function generateToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username
-  };
-  const options = {
-    expiresIn: "1d"
-  };
-  return jwt.sign(payload, secrets.jwtSecret, options);
-}
 
 module.exports = router;
